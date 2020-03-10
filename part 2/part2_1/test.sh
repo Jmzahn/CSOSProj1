6fd4040144d01545f083f8e2290ae410d5b27c7e
Jacob Zahn <jmzahn@satx.rr.com>
2020-03-09 20:03:35 -0500

    update test scripts

--- part 2/part2_1/test.sh
+++ part 2/part2_1/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\nTest with 2\n"
-./part2_1 2
+./part2_1 2 > part2_1_2_out
 echo -e "\nTest with 4\n"
-./part2_1 4
+./part2_1 4 > part2_1_4_out
 echo -e "\nTest with 8\n"
-./part2_1 8
+./part2_1 8 > part2_1_8_out
--- part 2/part2_2/test.sh
+++ part 2/part2_2/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\nTest with 2\n"
-./part2_2 2
+./part2_2 2 > part2_2_2_out
 echo -e "\nTest with 4\n"
-./part2_2 4
+./part2_2 4 > part2_2_4_out
 echo -e "\nTest with 8\n"
-./part2_2 8
+./part2_2 8 > part2_2_8_out
