89c1257fa9338bc37a6ede7c84f7a9affb573849
Jacob Zahn <jmzahn@satx.rr.com>
2020-03-09 20:01:01 -0500

    update code

--- part 3/part3.c
+++ part 3/part3.c
@@ -1,33 +1,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
-void print_hello();
+void *consumer(void *ptr);
 
-void processCreator(int numP, int numC);
+void *producer(void *ptr);
 
-void consumer();
-
-void producer();
-
-int produce_item();
-
-void insert_item(int item);
-
-int remove_item();
-
-void wakeup();
-
+#define N 100
 pthread_mutex_t task_queue_lock;
-int task_available;
+pthread_cond_t condc, condp;
+int buffer = 0;
 
 int main(int argc, char *argv[])
 {
-    task_available = 0;
-    pthread_mutex_init(&task_queue_lock, NULL);
-
+    
     if(argc != 3){
         return(-1);
     }
@@ -39,89 +25,49 @@ int main(int argc, char *argv[])
         numP = conv;
         conv = strtol(argv[2], &tempChar, 10);
         numC = conv;
-        processCreator(numP, numC);
+        pthread_t pro[numP];
+        pthread_t con[numC];
+        pthread_mutex_init(&task_queue_lock, 0);
+        pthread_cond_init(&condc, 0);
+        pthread_cond_init(&condp, 0);
+        for(int i = 0; i<numC; i++){
+            pthread_create(&con[i],NULL,consumer,NULL);
+            pthread_join(con[i],0);
+        }
+        for(int i = 0; i<numP; i++){
+            pthread_create(&pro[i],NULL,producer,NULL);
+            pthread_join(pro[i],0);
+        }
+        pthread_cond_destroy(&condc);
+        pthread_cond_destroy(&condp);
+        pthread_mutex_destroy(&task_queue_lock);
     }
 }
 
-void print_hello()
+void *consumer(void *ptr)
 {
-    int processID;
-    int parentProcessID;
-    processID = getpid();
-    parentProcessID = getppid();
-    printf("Hello from process: %d, my parent is: %d\n",processID,parentProcessID);
-}
+    int i;
 
-void processCreator(int numP, int numC)
-{
-    pid_t waits[numP];
-    pid_t status[numP];
-    for(int i = 0; i<numP; i++)
-    {
-        status[i] = fork();
-        if(status[i] != 0)
-        {
-            waits[i] = wait(NULL);
-            break;
-        }
+    for(i=1;i<=N;i++){
+        pthread_mutex_lock(&task_queue_lock);
+        while(buffer==0) pthread_cond_wait(&condc, &task_queue_lock);
+        buffer = 0;
+        pthread_cond_signal(&condp);
+        pthread_mutex_unlock(&task_queue_lock);
     }
-    print_hello();
-    exit(0);
-
-    /*for(int i = numP-1; i>=0; i--)
-    {
-        if(status[i] == 0)
-        {
-            print_hello();
-            exit(0);
-        }
-    }*/
+    pthread_exit(0);
 }
 
-#define N 100
-int count = 0;
-
-void *consumer()
+void *producer(void *ptr)
 {
-    int item;
-    int removed;
+    int i;
 
-    while(TRUE){
-        removed = 0;
-        while(removed == 0){
-            pthread_mutex_lock(&task_queue_lock);
-            if(task_available == 1){
-                item = remove_item();
-                task_available = 0;
-                removed = 1;
-            }
-            pthread_mutex_unlock(&task_queue_lock);
-        }
+    for(i=1;i<=N;i++){
+        pthread_mutex_lock(&task_queue_lock);
+        while(buffer!=0) pthread_cond_wait(&condp, &task_queue_lock);
+        buffer = i;
+        pthread_cond_signal(&condc);
+        pthread_mutex_unlock(&task_queue_lock);
     }
-}
-
-void *producer()
-{
-    int item;
-    int inserted;
-    int total = 64;
-    int x = 0;
-    while(x<total){
-        inserted = 0;
-        item = produce_item();
-        while(inserted == 0){
-            pthread_mutex_lock(&task_queue_lock);
-            if (task_available == 0){
-                insert_item(item);
-                task_available = 1;
-                inserted = 1;
-            }
-            pthread_mutex_unlock(&task_queue_lock);
-        }
-    }
-}
-
-int produce_item()
-{
-    
+    pthread_exit(0);
 }
